6f312da478b72ddb6e275cfaea00c8a511eb6542
Ayush Gupta <ayushgupta304@gmail.com>
2021-05-24 16:19:03 +0530

    Basic edits

--- lib/models/network_data.dart
+++ lib/models/network_data.dart
@@ -1,19 +1,19 @@
 import 'package:ask_quotation_customer/company_data/company_data.dart';
 
-class URLNetwork{
-  urlNetwork(){
-    var url = '127.0.0.1:5000';
+class URLNetwork {
+  // urlNetwork(){
+  //   var url = '127.0.0.1:5000';
+  //   return url;
+  // }
+  urlNetwork() {
+    var url = '10.0.2.2:5000';
     return url;
   }
-// urlNetwork() {
-//   var url = '10.0.2.2:5000';
-//   return url;
-// }
 }
 
 String uri = URLNetwork().urlNetwork();
 
-requestUrl(var requestEndPoint){
+requestUrl(var requestEndPoint) {
   var requestUrl = Uri.http(uri, '$requestEndPoint');
   return requestUrl;
 }
@@ -22,22 +22,34 @@ var customerRegistration = requestUrl('kaizen/api/customer/register');
 var customerLogin = requestUrl('kaizen/api/customer/login');
 var currentCustomerDetails = requestUrl('kaizen/api/customer/currentUser');
 var customerRefreshToken = requestUrl('kaizen/api/customer/refreshToken');
-var customerEmailVerification = requestUrl('kaizen/api/customer/emailVerification');
-var customerMobileVerification = requestUrl('kaizen/api/customer/contactVerification');
+var customerEmailVerification =
+    requestUrl('kaizen/api/customer/emailVerification');
+var customerMobileVerification =
+    requestUrl('kaizen/api/customer/contactVerification');
 
-var requestCompanyDataAccess = requestUrl('kaizen/api/bothAdmin/allowingCustomer');
+var requestCompanyDataAccess =
+    requestUrl('kaizen/api/bothAdmin/allowingCustomer');
 
-var addItemToWishList = requestUrl('kaizen/api/customer/addToWishlist/$companyUsername');
+var addItemToWishList =
+    requestUrl('kaizen/api/customer/addToWishlist/$companyUsername');
 var deleteFromWishList = requestUrl('kaizen/api/customer/deleteItemWishlist');
-var updateWishListItemQuantity = requestUrl('kaizen/api/customer/updateQtyWishlist');
+var updateWishListItemQuantity =
+    requestUrl('kaizen/api/customer/updateQtyWishlist');
 
-getProducts(String category, String page){
-  var getProducts = requestUrl('kaizen/api/customer/$companyUsername/$category/$page');
+getProducts(String category, String page) {
+  var getProducts =
+      requestUrl('kaizen/api/customer/$companyUsername/$category/$page');
   return getProducts;
 }
-var getAllCategories = requestUrl('kaizen/api/customer/getAllCategory/$companyUsername');
 
-var homePageBannerImage = requestUrl('/kaizen/api/customer/getHomePageBannerImage/$companyUsername');
-var getWishlistLength = requestUrl('/kaizen/api/customer/wishlistLength/$companyUsername');
-var checkCompanyStatusBeforeLogin = requestUrl('/kaizen/api/customer/checkCompanyStatus/$companyUsername');
-var getAllPopularProducts = requestUrl('/kaizen/api/customer/getPopularProducts/$companyUsername');
+var getAllCategories =
+    requestUrl('kaizen/api/customer/getAllCategory/$companyUsername');
+
+var homePageBannerImage =
+    requestUrl('/kaizen/api/customer/getHomePageBannerImage/$companyUsername');
+var getWishlistLength =
+    requestUrl('/kaizen/api/customer/wishlistLength/$companyUsername');
+var checkCompanyStatusBeforeLogin =
+    requestUrl('/kaizen/api/customer/checkCompanyStatus/$companyUsername');
+var getAllPopularProducts =
+    requestUrl('/kaizen/api/customer/getPopularProducts/$companyUsername');
